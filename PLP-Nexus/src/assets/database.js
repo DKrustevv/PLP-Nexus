7259a92ef1a1239d402a7dc036f41627e3cecf08
Dimitar <mitkonkrastev@gmail.com>
2023-08-02 16:07:16 +0300

    refactor:database

--- PLP-Nexus/src/assets/database.js
+++ PLP-Nexus/src/assets/database.js
@@ -1,52 +1,264 @@
-const database={
-    "categories": [
-      {
-        "name": "Male Watches",
-        "watches": [
-          {
-            "name": "Lorus Watch",
-            "description": "A stylish Lorus watch with a silver-tone dial.",
-            "price": 129.99,
-            "image": "https://img.modivo.cloud/zoomapp(0/3/1/2/0312fc00596d4628f2d3b4a89b765321f43da997_01_4894138356728_is.jpg,webp)/lorus-chasovnik-rl419bx9-srebrist-4894138356728.webp",
-            "ratings": 4.2
-          },
-          {
-            "name": "ASGER DANTE Watch",
-            "description": "An elegant ASGER DANTE watch with a black leather strap.",
-            "price": 189.99,
-            "image": "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt766f8e16772a4645/60dc0e89bcc58b0f8f8a686e/asger_dante-20508-44mm.jpg?format=pjpg&auto=webp&quality=75%2C90&width=640",
-            "ratings": 4.5
-          },
-          {
-            "name": "Armani Exchange Watch",
-            "description": "A stylish Armani Exchange watch with a black dial and leather strap.",
-            "price": 249.99,
-            "image": "https://img2.ans-media.com/i/1080x1626/AA00-AKM0HP_MLC_F1.jpg?v=1665995562",
-            "ratings": 4.8
-          },
-          {
-            "name": "Roamer Watch",
-            "description": "A classic Roamer watch with a silver-tone dial and stainless steel bracelet.",
-            "price": 159.99,
-            "image": "https://img.modivo.cloud/zoomapp(9/f/c/5/9fc57f75ce080ff5f6d9ef2f5f4d1598dd4fa180_01_7640185780187_ts.jpg,webp)/roamer-chasovnik-elements-650810-41-45-50-srebrist.webp",
-            "ratings": 4.0
-          },
-          {
-            "name": "Skagen Watch",
-            "description": "A minimalist Skagen watch with a stainless steel case and mesh strap.",
-            "price": 119.99,
-            "image": "https://img2.ans-media.com/i/540x813/AA00-AKM0T2_88X_F1.jpg@webp?v=1572342622",
-            "ratings": 4.6
-          },
-          {
-            "name": "Fossil Watch",
-            "description": "A trendy Fossil watch with a turquoise dial and silicone strap.",
-            "price": 179.99,
-            "image": "https://img.eobuwie.cloud/eob_product_512w_512h(0/c/2/4/0c241df10252a52832d3bfec394afd35e649c0c2_01_4064092208894_pl.jpg,webp)/chasovnik-fossil-blue-fs5992-turquoise-turquoise-4064092208894.webp",
-            "ratings": 4.4
-          }
-        ]
-      }
-    ]
-  }
+const database = {
+  "categories": [
+    {
+      "name": "Male",
+      "watches": [
+        {
+          "name": "LORUS",
+          "description": "Watch RL419BX9 Silver",
+          "strap": "metal",
+          "price": 129.99,
+          "image": "https://img.modivo.cloud/zoomapp(0/3/1/2/0312fc00596d4628f2d3b4a89b765321f43da997_01_4894138356728_is.jpg,webp)/lorus-chasovnik-rl419bx9-srebrist-4894138356728.webp",
+          "ratings": 4.2
+        },
+        {
+          "name": "ASGER DANTE",
+          "description": "Watch Porie P598788 Silver",
+          "strap": "metal",
+          "price": 189.99,
+          "image": "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt766f8e16772a4645/60dc0e89bcc58b0f8f8a686e/asger_dante-20508-44mm.jpg?format=pjpg&auto=webp&quality=75%2C90&width=640",
+          "ratings": 4.5
+        },
+        {
+          "name": "ARMANI EXCHANGE",
+          "description": "Watch Korps C19282 Black",
+          "strap": "metal",
+          "price": 249.99,
+          "image": "https://img2.ans-media.com/i/1080x1626/AA00-AKM0HP_MLC_F1.jpg?v=1665995562",
+          "ratings": 4.8
+        },
+        {
+          "name": "ROAMER",
+          "description": "Watch Superior 508837 Silver",
+          "strap": "metal",
+          "price": 159.99,
+          "image": "https://img.modivo.cloud/zoomapp(9/f/c/5/9fc57f75ce080ff5f6d9ef2f5f4d1598dd4fa180_01_7640185780187_ts.jpg,webp)/roamer-chasovnik-elements-650810-41-45-50-srebrist.webp",
+          "ratings": 4.0
+        },
+        {
+          "name": "SKAGEN",
+          "description": "Watch Grenen SKW6874 Leatcher",
+          "strap": "leather",
+          "price": 119.99,
+          "image": "https://img2.ans-media.com/i/540x813/AA00-AKM0T2_88X_F1.jpg@webp?v=1572342622",
+          "ratings": 4.6
+        },
+        {
+          "name": "FOSSIL",
+          "description": "Watch Blue FS5995 Turquoise",
+          "strap": "silicone",
+          "price": 179.99,
+          "image": "https://img.eobuwie.cloud/eob_product_512w_512h(0/c/2/4/0c241df10252a52832d3bfec394afd35e649c0c2_01_4064092208894_pl.jpg,webp)/chasovnik-fossil-blue-fs5992-turquoise-turquoise-4064092208894.webp",
+          "ratings": 4.4
+        },
+        {
+          "name": "GUESS",
+          "description": "Watch Idol GW0503G4 Brown",
+          "strap": "leather",
+          "price": 139.99,
+          "image": "https://img.modivo.cloud/product(7/0/7/4/70749d026c8af091a99e086444bc6be1a5b3c345_01_0091661534690_AZ.jpg,webp)/guess-chasovnik-idol-gw0503g4-kafiav.webp",
+          "ratings": 4
+        },
+        {
+          "name": "MICHAEL KORS",
+          "description": "Watch Runway MK9105 Silver",
+          "strap": "metal",
+          "price": 239.99,
+          "image": "https://img.modivo.cloud/product(7/3/b/4/73b4bc04ed7cd03c940dff8b9922f7ef238d15e0_01_4064092215540_st.jpg,webp)/michael-kors-chasovnik-runway-mk9105-srebrist-4064092215540.webp",
+          "ratings": 5
+        },
+        {
+          "name": "D1 MILANO",
+          "description": "Watch D1-SKBJ11 Silver",
+          "strap": "metal",
+          "price": 1239.99,
+          "image": "https://img.modivo.cloud/product(1/8/6/e/186e1647ee0da5b1216be2ebcd01886d3766bd35_01_0716053752703_az,webp)/d1-milano-chasovnik-d1-skbj11-siv.webp",
+          "ratings": 4.8
+        },
+        {
+          "name": "ORIENT",
+          "description": "WATCH AA0811E19B Silver",
+          "strap": "metal",
+          "price": 339.99,
+          "image": "https://img.modivo.cloud/product(7/c/c/d/7ccd859cceac98ca40bfa12a547e19fdc86b4bb7_1_4942715028602_AZ.jpg,webp)/orient-chasovnik-aa0811e19b-srebrist.webp",
+          "ratings": 4.4
+        },
+        {
+          "name": "ORIENT",
+          "description": "Watch RA-AA0B01G19B Silver",
+          "strap": "metal",
+          "price": 299.99,
+          "image": "https://img.modivo.cloud/product(f/5/f/7/f5f72a025e943f7f5fcac42ca4934c9be7009742_1_4942715023218_AZ.jpg,webp)/orient-chasovnik-ra-aa0b01g19b-srebrist.webp",
+          "ratings": 3.1
+        },
+        {
+          "name": "ORIENT",
+          "description": "Watch RA-AK0310S10B Silver",
+          "strap": "leather",
+          "price": 299.99,
+          "image": "https://img.modivo.cloud/product(6/b/6/1/6b61983241b4ea28e1ff1d2210e5221508008bed_1_4942715028176_AZ.jpg,webp)/orient-chasovnik-ra-ak0310s10b-srebrist.webp",
+          "ratings": 4.1
+        },
+        {
+          "name": "ORIENT",
+          "description": "Watch RA-AR0002B10B Silver",
+          "strap": "metal",
+          "price": 399.99,
+          "image": "https://img.modivo.cloud/product(f/5/1/c/f51cbc2bf76422a0a18f64817f38c15dfb29c2d4_01_4942715013530_pl.jpg,webp)/orient-chasovnik-ra-ar0002b10b-srebrist.webp",
+          "ratings": 4.3
+        },
+        {
+          "name": "ORIENT",
+          "description": "Watch RA-AA0812L19B Silver",
+          "strap": "metal",
+          "price": 349.99,
+          "image": "https://img.modivo.cloud/product(6/5/8/c/658c45f5fccae35c18020ec7e6287806fc80ef99_1_4942715028619_AZ.jpg,webp)/orient-chasovnik-ra-aa0812l19b-srebrist.webp",
+          "ratings": 4.3
+        },
+        {
+          "name": "ADIDAS ORIGINALS",
+          "description": "Watch Edition Two AOFH23006 Silver",
+          "strap": "silicone",
+          "price": 149.99,
+          "image": "https://img.modivo.cloud/product(f/6/1/e/f61e444cb4bc3c9183b8bc6ec761dec407aa8348_01_0194366250367_is.jpg,webp)/adidas-originals-chasovnik-edition-two-watch-aofh23006-srebrist.webp",
+          "ratings": 4
+        },
+        {
+          "name": "ADIDAS ORIGINALS",
+          "description": "Watch Code One Watch AOSY23022 Blue",
+          "strap": "silicone",
+          "price": 109.99,
+          "image": "https://img.modivo.cloud/product(e/5/3/f/e53f788c261a92955f64dc44ce98bcfb3b7b8814_01_0194366250749_mt.jpg,webp)/adidas-originals-chasovnik-code-one-watch-aosy23022-srebrist.webp",
+          "ratings": 4.3
+        },
+        {
+          "name": "ADIDAS ORIGINALS",
+          "description": "Watch Edition One Watch AOFH23010 Silver",
+          "strap": "metal",
+          "price": 139.99,
+          "image": "https://img.modivo.cloud/product(3/a/4/5/3a4583dd2b2a11072e3502ac9de6a5383e4e9c9c_01_0194366250398_is.jpg,webp)/adidas-originals-chasovnik-edition-one-watch-aofh23010-srebrist.webp",
+          "ratings": 3.7
+        },
+        {
+          "name": "DIESEL",
+          "description": "Watch DZ7478 White",
+          "strap": "silicone",
+          "price": 339.99,
+          "image": "https://img.modivo.cloud/product(a/d/a/0/ada095e6dc14bc6363fec1e1362eb56f53220614_01_4064092209815_bs.jpg,webp)/diesel-chasovnik-dz7478-bial-4064092209815.webp",
+          "ratings": 5
+        },
+        {
+          "name": "DIESEL",
+          "description": "Watch DZ7479 Golden",
+          "strap": "metal",
+          "price": 1339.99,
+          "image": "https://img.modivo.cloud/product(6/a/9/0/6a9067262f919c7e2a0487d793f6d229e085bf9a_01_4064092209822_bs.jpg,webp)/diesel-chasovnik-dz7479-zlatist-4064092209822.webp",
+          "ratings": 4
+        },
+        {
+          "name": "DIESEL",
+          "description": "Watch MS9 DZ1865 Golden",
+          "strap": "metal",
+          "price": 539.99,
+          "image": "https://img.modivo.cloud/product(2/c/e/3/2ce3dbb6ee707c1072b11e6280386ff1afd78fde_4051432070174_main,webp)/diesel-chasovnik-ms9-dz1865-zlatist.webp",
+          "ratings": 0
+        },
+        {
+          "name": "DIESEL",
+          "description": "Watch Spiked DZ4609 Black",
+          "strap": "metal",
+          "price": 249.99,
+          "image": "https://img.modivo.cloud/product(f/7/a/d/f7ad755c46722cc914ddb4f996c026320e5e1f0c_01_4064092140682_az.jpg,webp)/diesel-chasovnik-spiked-dz4609-cheren.webp",
+          "ratings": 5
+        },
+        {
+          "name": "DIESEL",
+          "description": "Watch Mega Chief DZ431 Black",
+          "strap": "metal",
+          "price": 249.99,
+          "image": "https://img.modivo.cloud/product(a/4/1/3/a413b0006e0bf7e2096d90f2cd3350f0c4f66127_dz4318_main_macys_4053858226791,webp)/diesel-chasovnik-mega-chief-dz4318-cheren.webp",
+          "ratings": 5
+        }
+      ]
+    },
+    {
+      "name": "Female",
+      "watches": [
+        {
+          "name": "ADIDAS ORIGINALS",
+          "description": "Watch Project Two AOST23047 White",
+          "strap": "silicone",
+          "price": 100.99,
+          "image": "https://img.modivo.cloud/product(7/a/1/2/7a12f62a803223ad4bec833ea5fe377c5e8e05e0_01_0194366250497_ki.jpg,webp)/adidas-originals-chasovnik-project-two-watch-aost23047-bial.webp",
+          "ratings": 5
+        },
+        {
+          "name": "ADIDAS ORIGINALS",
+          "description": "Watch Project Two AOST23047 Blue",
+          "strap": "silicone",
+          "price": 100.99,
+          "image": "https://img.modivo.cloud/product(4/6/7/5/4675d3e93f7bd1af103c17dd92da763e2ec25c7b_01_0194366250510_ks.jpg,webp)/adidas-originals-chasovnik-project-two-watch-aost23049-sin.webp",
+          "ratings": 5
+        }, 
+        {
+          "name": "FOSSIL",
+          "description": "Watch Riley ES3202 Silver",
+          "strap": "metal",
+          "price": 110.99,
+          "image": "https://img.modivo.cloud/product(2/a/4/f/2a4fcedef1c95e1d611cc4959d1c7ab3ecb496d5_4051432733017.jpg,webp)/fossil-chasovnik-riley-es3202-srebrist.webp",
+          "ratings": 5
+        },
+        {
+          "name": "FOSSIL",
+          "description": "Watch Riley ES3204 Silver",
+          "strap": "metal",
+          "price": 110.99,
+          "image": "https://img.modivo.cloud/product(7/0/6/6/7066ef8969376ae16f5653068d43f5a87b4b8fa4_4051432733031_1.jpg,webp)/fossil-chasovnik-riley-es3204-srebrist.webp",
+          "ratings": 5
+        },
+        {
+          "name": "D1 MILANO",
+          "description": "Watch DTRJ01 Black",
+          "strap": "silicone",
+          "price": 310.99,
+          "image": "https://img.modivo.cloud/product(2/3/6/6/2366802900f58cc1661a56ea18c82a80cdc43746_01_0716053753441_MG.jpg,webp)/d1-milano-chasovnik-dtrj01-cheren-0716053753441.webp",
+          "ratings": 5
+        },
+        {
+          "name": "D1 MILANO",
+          "description": "Watch PHBJ02 Electric",
+          "strap": "silicone",
+          "price": 300.99,
+          "image": "https://img.modivo.cloud/product(c/9/f/c/c9fc7b22f1ce3d7d58eb819fff4d01d6e439a070_01_0716053753991_mf.jpg,webp)/d1-milano-chasovnik-phbj02-elektrikov-0716053753991.webp",
+          "ratings": 5
+        },
+        {
+          "name": "D1 MILANO",
+          "description": "Watch SQBJ01 Silver",
+          "strap": "metal",
+          "price": 370.99,
+          "image": "https://img.modivo.cloud/product(c/5/8/d/c58d382e2ec9ac73be7f9afbe4603b7566189031_01_0716053753946_mk.jpg,webp)/d1-milano-chasovnik-sqbj01-srebrist-0716053753946.webp",
+          "ratings": 5
+        },
+        {
+          "name": "MICHAEL KORS",
+          "description": "Watch Lexington MK7378 Golden",
+          "strap": "metal",
+          "price": 570.99,
+          "image": "https://img.modivo.cloud/product(1/c/d/6/1cd6e27c4db8a1ce0aa681b2f205269ee97bcbc9_01_4064092213997_kt.jpg,webp)/michael-kors-chasovnik-lexington-mk7378-zlatist-4064092213997.webp",
+          "ratings": 5
+        },
+        {
+          "name": "MICHAEL KORS",
+          "description": "Watch Emery MK4640 Golden",
+          "strap": "metal",
+          "price": 1570.99,
+          "image": "https://img.modivo.cloud/product(c/8/6/c/c86c4edaaf7edf9f1e20e2e2b62c4504a7f30bb0_01_4064092115864_ph.jpg,webp)/michael-kors-chasovnik-emery-mk4640-zlatist-4064092115864.webp",
+          "ratings": 5
+        },
+      ]
+    }
+  ]
+}
+
   export default database
