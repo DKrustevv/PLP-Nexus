683716afa19b7cf4a2589d7e473f1d177a871be3
Dimitar <mitkonkrastev@gmail.com>
2023-08-09 15:58:33 +0300

    add:navbar component

--- PLP-Nexus/src/components/NavBar/NavBar.jsx
+++ PLP-Nexus/src/components/NavBar/NavBar.jsx
@@ -1,41 +1,87 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { useState } from 'react';
+import PropTypes from 'prop-types'; 
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  Divider,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Logo from '../../assets/images/239-2398382_logo-store-png-clothing-shop-logo-png.png';
-import './NavBar.css'; 
+import Logo from '../../assets/images/shopunder.png';
+import './NavBar.css';
+import FilterMenu from '../FilterMenu/FilterMenu';
 
-const Navbar = () => {
+const Navbar = ({ handleSetFilteredWatches }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const [selectedStraps, setSelectedStraps] = useState([]);
+  const [selectedGenders, setSelectedGenders] = useState([]);
+  const [priceRange, setPriceRange] = useState([0, 2000]); 
+  const [minRating, setMinRating] = useState(0); 
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleApplyFilters = () => {
+    setIsMenuOpen(false);
+    handleSetFilteredWatches(selectedStraps, selectedGenders, priceRange, minRating);
+  };
+
+  const filterOptions = ['metal', 'leather', 'silicone']; 
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleMenuClick}
-        >
-          <MenuIcon />
-        </IconButton>
-        <img src={Logo} alt="Logo" className="logo" />
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        </Typography>
-        <IconButton color="inherit">
-          <ShoppingCartIcon />
-        </IconButton>
-       
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">About</Button>
-        <Button color="inherit">Contact</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar
+        position="sticky"
+        sx={{ backgroundColor: 'white', color: 'black', width: '100%' }}
+      >
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={Logo} alt="Logo" className="logo" />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
+          <IconButton color="inherit">
+            <ShoppingCartIcon sx={{ fontSize: 42 }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
+        <List>
+          <ListItem>
+            <FilterMenu
+              options={filterOptions}
+              selectedGenders={selectedGenders}
+              selectedStraps={selectedStraps}
+              priceRange={priceRange}
+              minRating={minRating}
+              setSelectedStraps={setSelectedStraps}
+              setSelectedGenders={setSelectedGenders}
+              setPriceRange={setPriceRange}
+              setMinRating={setMinRating}
+              handleApplyFilters={handleApplyFilters}
+            />
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+    </>
   );
+};
+
+// Prop validation
+Navbar.propTypes = {
+  handleSetFilteredWatches: PropTypes.func.isRequired,
 };
 
 export default Navbar;
